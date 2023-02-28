44b56d11487b4748396aba6ae4e30ff1fa39ffa4
xayalahmad <khayall.ahmadli@gmail.com>
2023-02-28 17:33:36 +0400

    created accordion

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Contact from '../Contact/Contact';
+import Accordion from './Accordion/Accordion'
 export default function Home()
 {
 
 
     return (
-        
+        <>
+        <Accordion/>
         <Contact/>
+        </>
     )
 }
